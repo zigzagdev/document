e4aeb59c971bb1fcb0a9846ec74a52dbe0dcbf1f
zigzagdev <msts.oo0131@gmail.com>
2023-05-23 23:04:12 +0900

    postMigrationFile_add

---
+++ src/php/database/migrations/2023_05_23_225859_posts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->text('description');
+            $table->integer('heritage_id')->unsigned();
+            $table->foreign('heritage_id')->references('id')
+                ->on('heritages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
